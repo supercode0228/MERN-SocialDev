56c8ab5e5ad7dc2b860fb5fab11275b7fd54cbe8
Angel0228 <supercode@Alexey.local>
2018-12-28 10:05:43 -0500

    Set the routes basically.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+//Routes Define
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 //DB Config
@@ -13,6 +18,11 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
