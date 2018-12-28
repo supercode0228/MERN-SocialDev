3d6ea34e6d5a059ade0f7ec08f2e7cbc47f1a649
Angel0228 <supercode@Alexey.local>
2018-12-28 09:39:55 -0500

    set Mongo Database

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://admin:admin123@ds145184.mlab.com:45184/devconnector"
+};
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!"));
 
 const port = process.env.PORT || 5000;
 
