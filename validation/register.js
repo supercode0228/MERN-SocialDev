6f93482a6343a111a85f19b48b1f1eb09b0f066c
Angel0228 <supercode@Alexey.local>
2019-01-15 00:43:00 -0500

    auth completed

--- validation/register.js
+++ validation/register.js
@@ -29,6 +29,10 @@ module.exports = function validateRegisterInput(data) {
     errors.password = "Password field is required";
   }
 
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm Password field is required";
+  }
+
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = "Password must be at least 6 characters";
   }
